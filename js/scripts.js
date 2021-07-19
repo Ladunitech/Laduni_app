28cabd42e6f9bae1a613c06655e206a645977fa7
sitiinsyirah <siti.insyirah@laduni.tech>
2021-07-19 10:47:20 +0800

    html source code

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function ToggleNavBar() {
+  let element = document.getElementsByClassName('navbar-toggler')[0] ;
+  if (element.getAttribute('aria-expanded') == 'true') {
+    element.click();
+  }
+}
+
+ function onSubscribe() {
+    window.open("https://tech.us6.list-manage.com/subscribe/post?u=9827b55b2af4c603f84d06a28&amp;id=93e18d797b", "_blank");
+  }
